7fba041d681159538673484359100f42dd731bdc
Nishant Srivastava <20250366+nishi7409@users.noreply.github.com>
2020-06-10 20:24:58 +0530

    pretty much done
    pretty much done with the mockup, don't really know how to trigger click events to display each different pathway though.  maybe it's really simple and I'm overthinking it right now

--- frontend/src/components/Pathway.vue
+++ frontend/src/components/Pathway.vue
@@ -3,7 +3,7 @@
     <!-- left panel (with the pathway names) -->
     <i v-if=drawer class="material-icons" @click="drawer=!drawer">keyboard_arrow_left</i>
     <i v-else class="material-icons" @click="drawer=!drawer">keyboard_arrow_right</i>
-    <v-navigation-drawer v-model="drawer" color="#D3D3D3" height="450" width="256" class="mt-4">
+    <v-navigation-drawer v-model="drawer" color="#D3D3D3" height="450" width="14%" class="mt-4">
       <ul class="mt-4 mr-2">
         <v-btn small depressed class="mt-2 text-wrap" v-for="item in items" :key="item.name">
           {{ item.name }}
@@ -34,15 +34,12 @@
       
       <!-- pathway courses & course information -->
       <div id="bottom_panel">
-        <v-card id="bottom_panel" color="#ecf0f5">
+        <v-card id="bottom_panel" color="#e5e8ed">
 
           <!-- all courses possible for path -->
           <v-navigation-drawer v-modoel="drawer" color="#e5e8ed" height="100%" width="256" class="mt-4">
             <ul class="mt-4 mr-2">
-              <v-btn color="#e5e8ed" small depressed class="mt-2 text-wrap" v-for="item in items" :key="item.name">
-                {{ item.courses[0].course_name }}
-              </v-btn>
-              <v-btn color="#e5e8ed" small depressed class="mt-2 text-wrap" v-for="item in items" :key="item.name">
+              <v-btn color="#e5e8ed" small depressed class="mt-2 text-wrap" v-for="item in items" :key="item.course_name">
                 {{ item.courses[0].course_name }}
               </v-btn>
               <br>
@@ -51,8 +48,24 @@
 
           <!-- course information -->
           <v-card-title id="course_information_headline" class="font-weight-black">Course Information</v-card-title>
-          <v-card-title id="actual_course_information" class="font-weight-regular">This course is an introduction to the philosophy of mind and cognitive science. Students meet in small sections to have class discussions and debates about questions like: What are minds? Are minds physical or non-physical? Do humans have free will? Does reliance on technology turn humans into cyborgs? How close are humans to building an intelligent robot or machine? Do we want this to be a goal? Students will learn how make a philosophical argument, and how to express them in writing or through an oral presentation. This course is communication intensive.</v-card-title>
-          
+
+          <!-- buttons -->
+          <div id="chip_info">
+            <v-chip class="ma-2" color="red" text-color="white">Required</v-chip>
+            <v-chip class="ma-2" color="green" text-color="white">HASS Inquiry</v-chip>
+            <v-chip class="ma-2" color="red" text-color="white">Major Restricted</v-chip>
+            <v-chip class="ma-2" color="red" text-color="white">Gateway Course</v-chip>
+          </div>
+
+          <v-card-title id="actual_course_information" class="font-weight-regular">
+            This course is an introduction to the philosophy of mind and cognitive science. Students meet in small sections to have class discussions and debates about questions like: What are minds? Are minds physical or non-physical? Do humans have free will? Does reliance on technology turn humans into cyborgs? How close are humans to building an intelligent robot or machine? Do we want this to be a goal? Students will learn how make a philosophical argument, and how to express them in writing or through an oral presentation. This course is communication intensive.
+          </v-card-title>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text color="primary">
+              Select
+            </v-btn>
+          </v-card-actions>
         </v-card>
       </div>
     </div>
@@ -76,13 +89,39 @@ export default {
               hass_inquiry: true,
               major_restricted: false,
               gateway_course: false,
+              required: false
             },
             {
-              course_name: "Alex",
-              course_description: "This course is an introduction to the philosophy of mind and cognitive science. Students meet in small sections to have class discussions and debates about questions like: What are minds? Are minds physical or non-physical? Do humans have free will? Does reliance on technology turn humans into cyborgs? How close are humans to building an intelligent robot or machine? Do we want this to be a goal? Students will learn how make a philosophical argument, and how to express them in writing or through an oral presentation. This course is communication intensive.",
+              course_name: "AI and Society",
+              course_description: "N/A",
               hass_inquiry: true,
               major_restricted: false,
               gateway_course: false,
+              required: false
+            },
+            {
+              course_name: "Are Humans Rational?",
+              course_description: "The Aristotelian dictum that we are rational animals is under severe attack these days. In fact, the previous sentence may seriously understate the situation: the dictum is perhaps outright rejected by many, if not most. From psychologists of reasoning and decision-making to behavioral economists to the “new atheists” (all groups whose message we will consider in this class), the onslaught is firmly underway, and fierce.",
+              hass_inquiry: true,
+              major_restricted: false,
+              gateway_course: false,
+              required: false
+            },
+            {
+              course_name: "Introduction to Cognitive Science",
+              course_description: "This course is an introduction to the new and quickly growing field of cognitive science. Cognitive Science is a highly interdisciplinary field of study of mind at the intersection of philosophy, psychology, computer science, neuroscience, linguistics, and anthropology. This is a communication-intensive course.",
+              hass_inquiry: true,
+              major_restricted: false,
+              gateway_course: false,
+              required: true
+            },
+            {
+              course_name: "More Courses",
+              course_description: "More course descriptions",
+              hass_inquiry: true,
+              major_restricted: false,
+              gateway_course: false,
+              required: false
             },
           ]
         },
@@ -158,16 +197,21 @@ position: absolute;
   width: 80%
 }
 
-
 #course_information_headline {
-  background:#ecf0f5;
+  background:#e5e8ed;
   position: absolute;
   top: 0px;
   left: 256px;  
 }
 
+#chip_info {
+    position: absolute;
+  top: 0%;
+  left: 65%; 
+}
+
 #actual_course_information {
-  background: #ecf0f5;
+  background: #e5e8ed;
   position: absolute;
   top: 40px;
   left: 256px;  
