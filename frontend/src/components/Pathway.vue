ef55e5a3bff6bcb14c6e10efd7e520f8ba9c7a9d
Nishant Srivastava <20250366+nishi7409@users.noreply.github.com>
2020-06-13 15:26:15 +0530

    update
    update before i begin destroying my work with a tutorial example implemented

--- frontend/src/components/Pathway.vue
+++ frontend/src/components/Pathway.vue
@@ -1,11 +1,12 @@
 <template>
   <v-app>
     <!-- left panel (with the pathway names) -->
+    <!-- parent for pathway description & pathway courses -->
     <i v-if=drawer class="material-icons" @click="drawer=!drawer">keyboard_arrow_left</i>
     <i v-else class="material-icons" @click="drawer=!drawer">keyboard_arrow_right</i>
     <v-navigation-drawer v-model="drawer" color="#D3D3D3" height="450" width="14%" class="mt-4">
       <ul class="mt-4 mr-2">
-        <v-btn small depressed class="mt-2 text-wrap" v-for="item in items" :key="item.name">
+        <v-btn small block depressed class="mt-2 text-wrap" v-for="item in pathway_info" :key="item.name">
           {{ item.name }}
         </v-btn>
       </ul>
@@ -22,25 +23,30 @@
             Pathway Description
           </p>
 
-          <!-- pathway description -->
+          <!-- pathway description // THIS SHOULD CHANGE UPON CLICKING A NEW PATHWAY -->
+          <!-- child of parent (line 4) -->
           <p class="font-weight-medium">
-            Artificial Intelligence is quickly becoming pervasive in our lives. Study how Artificial Intelligence can benefit from concepts and ideas from cognitive science, and explore the ways in which Artificial Intelligence is changing our lives.
+            Pathway description
           </p>
         </v-card-text>
       </v-card>
       
       <br>
+
+      <!-- THIS SHOULD CHANGE (FIRST/SECOND/THIRD) ON STEPPER EVENT -->
       <p class="font-weight-bold"> Select a first course option </p>
       
-      <!-- pathway courses & course information -->
+      <!-- pathway courses & course information // THIS SHOULD CHANGE UPON NEW PATHWAY CLICK (NEW COURSES DISPLAYED)-->
       <div>
         <v-card width="100%" height="100%" color="#e5e8ed">
 
           <!-- all courses possible for path -->
+          <!-- child of parent (line 4) -->
+          <!-- parent for course descriptions -->
           <v-navigation-drawer v-modoel="drawer" color="#e5e8ed" height="100%" width="256" class="mt-4">
             <ul class="mt-4 mr-2">
-              <v-btn color="#e5e8ed" small depressed class="mt-2 text-wrap" v-for="item in items" :key="item.course_name">
-                {{ item.courses[0].course_name }}
+              <v-btn color="#e5e8ed" block depressed class="mt-2 text-wrap" v-for="item in pathway_info" :key="item.Courses[0]">
+                {{ item.Courses[0]}}
               </v-btn>
               <br>
             </ul>
@@ -49,12 +55,15 @@
           <!-- course information -->
           <v-card-title id="course_information_headline" class="font-weight-black">Course Information</v-card-title>
 
-          
+          <!-- THIS SHOULD CHANGE UPON CLICKING ON COURSE NAME  -->
+          <!-- child of parent (line 45) -->
           <v-card-title id="actual_course_information" class="font-weight-regular">
-            This course is an introduction to the philosophy of mind and cognitive science. Students meet in small sections to have class discussions and debates about questions like: What are minds? Are minds physical or non-physical? Do humans have free will? Does reliance on technology turn humans into cyborgs? How close are humans to building an intelligent robot or machine? Do we want this to be a goal? Students will learn how make a philosophical argument, and how to express them in writing or through an oral presentation. This course is communication intensive.
+            Course description
           </v-card-title>
           <v-card-actions>
             <v-spacer></v-spacer>
+
+            <!-- THIS SHOULD TRIGGER STEPPER EVENT -->
             <v-btn text color="primary">
               Select
             </v-btn>
@@ -68,79 +77,88 @@
 <script>
 
 export default {
+  // props to change things on the page on click
+  props: {
+    pathGrabber: String,
+    courseGrabber: String
+  },
   data() {
     return {
       drawer: true,
-      items: [
-        { 
-          name: 'Artificial Intelligence', 
-          pathway_description: `pathway description bro`,
-          courses: [
-            {
-              course_name: "Minds and Machines",
-              course_description: "This course is an introduction to the philosophy of mind and cognitive science. Students meet in small sections to have class discussions and debates about questions like: What are minds? Are minds physical or non-physical? Do humans have free will? Does reliance on technology turn humans into cyborgs? How close are humans to building an intelligent robot or machine? Do we want this to be a goal? Students will learn how make a philosophical argument, and how to express them in writing or through an oral presentation. This course is communication intensive.",
-              hass_inquiry: true,
-              major_restricted: false,
-              gateway_course: false,
-              required: false
-            },
-          ]
-        },
+      pathway_info: [
         {
-          name: 'Chinese Language',
-          pathway_description: `pathway description bro`,
-          courses: [
-            {
-              course_name: "Course Name1",
-              course_description: "Course description",
-              // boolean for special stuff  
-            }
-          ]
-        },
-        {
-          name: 'History',
-          pathway_description: `pathway description bro`,
-          courses: [
-            {
-              course_name: "Course name2",
-              course_description: "Course description",
-              // boolean for special stuff  
-            }
-          ]
-        },
-        {
-          name: 'Creative Design and Innovation',
-          pathway_description: `pathway description bro`,
-          courses: [
-            {
-              course_name: "Course name",
-              course_description: "Course description",
-              // boolean for special stuff  
-            }
-          ]
-        },
-        {
-          name: 'Economics',
-          pathway_description: `pathway description bro`,
-          courses: [
-            {
-              course_name: "Course name",
-              course_description: "Course description",
-              // boolean for special stuff  
-            }
-          ]
-        },
-        {
-          name: 'Electronic Arts',
-          pathway_description: `pathway description bro`,
-          courses: [
-            {
-              course_name: "Course name",
-              course_description: "Course description",
-              // boolean for special stuff  
-            }
-          ]
-        },
+            "id": 0,
+            "name": "Artifical Intelligence",
+            "desc": "Artificial Intelligence is quickly becoming pervasive in our lives. Study how Artificial Intelligence can benefit from concepts and ideas from cognitive science, and explore the ways in which Artificial Intelligence is changing our lives.",
+            "Courses": [
+              1972,
+              1140,
+              1235
+            ]
+          },
+          {
+            "id": 1,
+            "name": "Arts History, Theory, and Criticism",
+            "desc": "This pathway allows students to study a range of art history and media art histories and theories. This immersive study will give students a comprehensive view of how these histories have shaped our current day media and given rise to a variety of art making tools in our digital and computational era.",
+            "Courses": [
+              1080,
+              1300,
+              1170,
+              1700
+            ]
+          },
+          {
+            "id": 4,
+            "name": "Creative Design and Innovation",
+            "desc": "This pathway looks at creative design and innovation from various humanities, arts, and social science points of view. Students will learn about the cognitive and communicative principles behind design and innovation, the economic policies, markets, and other social institutions driving and shaping design and innovation, and how to engage in sustainable and socially responsible design and innovation for local and global impact.",
+            "Courses": [
+              1170,
+              1700,
+              1969,
+              1964
+            ]
+          },
+          {
+            "id": 6,
+            "name": "Economics",
+            "desc": "Study different types of theories and statistical methods used by economists. Students are prepared to gain a broad understanding of how consumers, firms and governments make decisions, and their implications.",
+            "Courses": [
+              1200
+            ]
+          },
+          {
+            "id": 7,
+            "name": "Economics of Banking and Finance",
+            "desc": "Study markets and policies in banking and finance. Students are prepared to use different types of mathematical models and econometric techniques used by economists to study the behavior of individuals, firms, and the economy.",
+            "Courses": [
+              1200
+            ]
+          },
+          {
+            "id": 8,
+            "name": "Economics of Biotech and Medical Markets",
+            "desc": "Study different types of markets related to pharmaceuticals, medical devices, and healthcare. Students are prepared to use theory, econometric models, and data to study the role of R&D, patents, innovation, and policies in healthcare markets, and in pharmaceuticals and medical devices.",
+            "Courses": [
+              1200
+            ]
+          },
+          {
+            "id": 9,
+            "name": "Economics of Policy and Regulations",
+            "desc": "Study different types of regulations and economic policy measures, and their impact on individuals, markets, and the economy. Students are prepared to use economic models, quantitative methods, and data to assess different types of regulations and policies, and their impact.",
+            "Courses": [
+              1200
+            ]
+          },
+          {
+            "id": 10,
+            "name": "Economics of Quantitative Modeling",
+            "desc": "Study different types of mathematical models and econometric techniques used in Economics. Students are prepared to use economic theory, and econometric and statistical methods to study behavior of individuals and firms in the economy.",
+            "Courses": [
+              1200
+            ]
+          },
+  
       ],
       pathway_button: true
     }
@@ -169,7 +187,7 @@ export default {
   background: #e5e8ed;
   position: absolute;
   left: 256px;
-  top: 15%;
+  top: 30%;
 
 }
 
@@ -183,3 +201,5 @@ li {
 }
 
 </style>
+
+
