6129271d170d66177c8a7be4178bbb7d90daed25
Nishant Srivastava <20250366+nishi7409@users.noreply.github.com>
2020-07-08 23:52:55 +0530

    deleted files
    removed all the junk (files the website didn't interact with)

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,28 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Inquiry from './components/Inquiry.vue'
-import Pathway from './components/Pathway.vue'
 import MyPathways from './components/MyPathways.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'pathway',
-      component: Pathway
-    },
-    {
-      path: '/inquiry',
-      name: 'inquiry',
-      component: Inquiry
-    },
-    {
-      path: '/pathway',
-      name: 'pathway',
-      component: Pathway
-    },
     {
       path: '/mypathways',
       name: 'mypathways',
