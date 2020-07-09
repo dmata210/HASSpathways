bdad79ede4e21d61f9d261adf5450cf20f1940a8
Nishant Srivastava <20250366+nishi7409@users.noreply.github.com>
2020-07-09 22:20:11 +0530

    activity ui
    Changed up the interface for the activity component

--- frontend/src/components/MyPathways.vue
+++ frontend/src/components/MyPathways.vue
@@ -1,55 +1,74 @@
 <template>
 
   <div>
-    <!-- <v-card class="mt-4 mb-4 mr-2 ml-2">
-
-      <v-card-text style="font-family: 'Muli', sans-serif; font-size: 18px">
-        The American Dream
-        <i class="fas fa-long-arrow-alt-right ml-8 mr-8"></i>
-        American History
-        <i class="fas fa-long-arrow-alt-right ml-8 mr-8"></i>
-        History of American Technology
-      </v-card-text>
-
-    </v-card>
-
-    <v-card class="mr-2 ml-2">
-
-      <v-card-text style="font-family: 'Muli', sans-serif; font-size: 18px">
-        Digital Filmmaking
-        <i class="fas fa-long-arrow-alt-right ml-8 mr-8"></i>
-        Sculpture I
-        <i class="fas fa-long-arrow-alt-right ml-8 mr-8"></i>
-        Sculpture 2
-      </v-card-text>
-
-    </v-card> -->
-
-    <!-- <v-banner single-line>
-      <v-chip class="ma-4" large>large chip</v-chip>
-      <v-chip class="ma-4" large>large chip</v-chip>
-      <v-chip class="ma-4" large>large chip</v-chip>
-    </v-banner> -->
     <center>
       <h1>Saved Courses</h1>
     </center>
 
-    <v-card>
+    <v-card flat>
       <v-card-title>
-        Pathway Name
+        Artificial Intelligence
       </v-card-title>
       <v-card-subtitle>
-        <v-btn depressed shaped x-large width="33%" color="pink" dark>Minds and Machines</v-btn>
-        <v-btn depressed shaped x-large width="33%" color="orange" dark>Artificial Intelligence and Society</v-btn>
-        <v-btn depressed shaped x-large width="33%" color="success" dark>Are Humans Rational?</v-btn>
+        <v-btn depressed shaped x-large width="33%" color="pink" dark @click="selectCourse('Minds and Machines')">
+          <v-icon>mdi-numeric-1</v-icon>
+          Minds and Machines</v-btn>
+        <v-btn depressed shaped x-large width="33%" color="orange" dark  @click="selectCourse('Artificial Intelligence and Society')">
+          <v-icon>mdi-numeric-2</v-icon>
+          Artificial Intelligence and Society</v-btn>
+        <v-btn depressed shaped x-large width="33%" color="success" dark  @click="selectCourse('Are Humans Rational?')">
+          <v-icon>mdi-numeric-3</v-icon>
+          Are Humans Rational?</v-btn>
       </v-card-subtitle>
       <v-card-text>
-        <v-card>
-          asd
+        <v-card width="100%" height="90%" outlined id="rounded-left">
+        
+          <!-- course name & important chips -->
+          <v-card-title v-if="specifiedCourse == 'None'">
+            Minds and Machines
+          </v-card-title>
+
+          <v-card-title v-else>
+            {{ this.specifiedCourse }}
+          </v-card-title>
+
+          <v-card-subtitle>
+            <v-chip small color="orange" class="mr-2 mt-2" text-color="white">IHSS 1140</v-chip>
+            <v-chip small color="red" class="mr-2 mt-2" text-color="white">Required</v-chip>
+            <v-chip small color="green" class="mr-2 mt-2" text-color="white">Major Restricted</v-chip>
+            <v-chip small color="green" class="mr-2 mt-2" text-color="white">Gateway Course</v-chip>
+            <v-chip small color="red" class="mr-2 mt-2" text-color="white">HASS Inquiry</v-chip>
+          </v-card-subtitle>
+
+          <v-divider></v-divider>
+
+          <!-- course description -->
+          <v-card-text class="font-weight-black">
+            Integrated with Chinese culture, students will learn all four types of language skills (listening, speaking, reading, and writing). After completing the Chinese pathway, students will be able to communicate in Chinese at their targeted proficiency levels and think critically and creatively with global and multicultural awareness.
+          </v-card-text>
         </v-card>
       </v-card-text>
     </v-card>
 
+
   </div>
+  
 
 </template>
+
+
+<script>
+export default {
+  props: ['path'],
+  data() {
+    return {
+      specifiedCourse: "None"
+    }
+  },
+  methods: {
+    selectCourse(course) {
+      this.specifiedCourse = course;
+    }
+  }
+}
+</script>
